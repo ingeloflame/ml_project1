f0a23f677668817b4c072144f424569154b99c31
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2023-12-13 01:14:56 +0500

    Add files via upload

---
+++ test_pi_hw3_maksimova.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from pi_hw3_maksimova import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_get_info():
+    response = client.get("/translate/")
+    assert response.status_code == 200
+    assert response.json() == [{"translation_text":"Ce n'était pas si simple."}]
