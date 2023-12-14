58d30c0c2ad11661153749515581580736da69ee
ilonautkevich <149751920+ilonautkevich@users.noreply.github.com>
2023-12-14 23:29:48 +0500

    Add files via upload

---
+++ test_hw3_popova.py
@@ -0,0 +1,16 @@
+Python 3.9.10 (v3.9.10:f2f3f53782, Jan 13 2022, 17:02:14) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================ RESTART: Shell ================================
+>>> 
+from fastapi.testclient import TestClient
+from hw3_popova import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
