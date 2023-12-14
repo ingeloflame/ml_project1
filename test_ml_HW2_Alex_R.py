d377a9b2e4fdd98c8b6d7813c1ed7a12495fabb8
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2023-12-14 21:55:49 +0500

    Update test_ml_HW2_Alex_R.py

--- test_ml_HW2_Alex_R.py
+++ test_ml_HW2_Alex_R.py
@@ -4,7 +4,7 @@ at = AppTest.from_file("ml_HW2_Alex_R.py")
 
 def test_load_image():
     at = AppTest.from_file("ml_HW2_Alex_R.py")
-    at.run() 
+    at.run(timeout=10) 
   #  at.file_uploader[0].input(open('test_img.png', 'rb')).run()
   #  assert not at.exception
 
