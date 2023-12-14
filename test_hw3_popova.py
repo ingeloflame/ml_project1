efb2ed71ccece22cd344263f8fd67b5cec3301ce
ilonautkevich <149751920+ilonautkevich@users.noreply.github.com>
2023-12-14 23:42:21 +0500

    Update test_hw3_popova.py

--- test_hw3_popova.py
+++ test_hw3_popova.py
@@ -1,9 +1,4 @@
-Python 3.9.10 (v3.9.10:f2f3f53782, Jan 13 2022, 17:02:14) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-================================ RESTART: Shell ================================
->>> 
+
 from fastapi.testclient import TestClient
 from hw3_popova import app
 
