d18279bf3c63aaca424e99f1583f977d0ea0b988
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2023-12-14 22:29:26 +0500

    Update test_pi_hw3_maksimova.py

--- test_pi_hw3_maksimova.py
+++ test_pi_hw3_maksimova.py
@@ -12,4 +12,4 @@ def test_read_main():
 def test_get_info():
     response = client.get("/translate/")
     assert response.status_code == 200
-    assert response.json() == [{"translation_text":"Ça ne fut pas si facile.."}]
+    assert response.json() == [{"translation_text":"Ça ne fut pas si facile."}]
